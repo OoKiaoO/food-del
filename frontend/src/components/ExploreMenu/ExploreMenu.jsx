9dad03236ed88ea191c35dd50e589b0bf8810099
Chiara Campalani <campalanichiara@gmail.com>
2024-05-16 11:15:51 +0900

    added logic to ExploreMenu component

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { menu_list } from '../../assets/assets'
 import './ExploreMenu.css'
 
 
-export const ExploreMenu = () => {
+export const ExploreMenu = ({category, setCategory}) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -12,8 +14,8 @@ export const ExploreMenu = () => {
       <div className='explore-menu-list'>
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className='explore-menu-list-item'>
-              <img src={item.menu_image} alt='' />
+            <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className='explore-menu-list-item'>
+              <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt='' />
               <p>{item.menu_name}</p>
             </div>
           )
