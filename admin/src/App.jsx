592f2b8ca155bbb2488b6e5d7aa01afe8350d107
Chiara Campalani <campalanichiara@gmail.com>
2024-05-19 16:56:21 +0900

    created structure of Admin page & Navbar & Sidebar comp

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
     </div>
   )
 }
