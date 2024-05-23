9ea12c95c7a415c0911f661196049a904affb452
Chiara Campalani <campalanichiara@gmail.com>
2024-05-23 17:53:04 +0900

    created logic to place an order & data to send to stripe api

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,60 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js"
-import stripe from "stripe"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order form frontend
 const placeOrder = async (req,res) => {
+  const frontend_url = "http://licalhost:5173";
+
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address
+    })
+    // saving order in db
+    await newOrder.save();
+    // clearing data stored in cart & resetting it
+    await userModel.findByIdAndUpdate(req.body.userId, {cartData:{}})
+    // creating requred data by stripe payment api
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "jpy",
+        product_data: {
+          name: item.name
+        },
+        unit_amount: item.price * 100 * 80
+      },
+      quantity: item.quantity
+    }))
+
+    line_items.push({
+      price_data: {
+        currency: "jpy",
+        product_data: {
+          name: "Delivery Charges"
+        },
+        unit_amount: 2*100*80
+      },
+      quantity: 1
+    })
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+    })
+
+    res.json({success: true, session_url: session.url})
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: "Error while connecting to Stripe api"})
+
+  }
 
 }
 
