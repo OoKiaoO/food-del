b0fdbcbfa34551260a565182fe8726174de82d9c
Chiara Campalani <campalanichiara@gmail.com>
2024-05-24 15:14:52 +0900

    created admin orders page & connected it to backend api

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,9 +5,9 @@ import Sidebar from './components/Sidebar/Sidebar'
 import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
-import Order from './pages/Order/Order'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
   const url = "http://localhost:4000";
@@ -22,7 +22,7 @@ const App = () => {
         <Routes>
           <Route path="/add" element={<Add url={url} />} />
           <Route path="/list" element={<List url={url} />} />
-          <Route path="/order" element={<Order url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import authMidlleware from "../middleware/auth.js"
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMidlleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMidlleware, userOrders);
+orderRouter.get("/list", listOrders);
 
 export default orderRouter;
