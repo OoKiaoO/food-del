78a1b65a409cd55698b8fca99e183c73d4ae0046
Chiara Campalani <campalanichiara@gmail.com>
2024-05-30 18:06:44 +0900

    fixed typo in img relative path

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -12,7 +12,7 @@ const FoodItem = ({id, name, price, description, image}) => {
   return (
     <div className='food-item' >
       <div className="food-item-img-container">
-        <img className='food-item-img' src={`public${image}`} alt='' />
+        <img className='food-item-img' src={image} alt='' />
         {
           !cartItems[id] 
           ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt='' /> 
